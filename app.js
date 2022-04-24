90d659919acf7ae7d538321ba831cf281053fd90
Chosen2730 <70141158+Chosen2730@users.noreply.github.com>
2022-04-24 01:41:29 +0100

    complete project framework

--- app.js
+++ app.js
@@ -8,7 +8,61 @@ const date = document.getElementById("date");
 const newDate = new Date().getFullYear();
 date.innerHTML = newDate;
 // ********** close links ************
+const navBar = document.querySelector("#nav");
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+const linksHeight = links.getBoundingClientRect().height;
+
+navToggle.addEventListener("click", function () {
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
 
 // ********** fixed navbar ************
+const topLink = document.querySelector(".top-link");
+const fixedNav = document.querySelector(".fixed-nav");
+
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navBar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navBar.classList.add("fixed-nav");
+  } else {
+    navBar.classList.remove("fixed-nav");
+  }
+  if (scrollHeight > 300) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = link.getAttribute("href").slice(1);
+    linksContainer.style.height = 0;
+    const element = document.getElementById(id);
+    const navHeight = navBar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navBar.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+  });
+});
